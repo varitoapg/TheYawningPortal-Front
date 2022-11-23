57820cf77bdd9deaf4d766bd8318a025edef650d
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-23 15:06:52 +0100

    Fix typo in mainStyleSizes and lightest and darkest Whites' properties

--- src/styles/mainStyleFonts.ts
+++ src/styles/mainStyleFonts.ts
@@ -4,8 +4,8 @@ const mainStyleFonts = {
   mainFont: "Karla, sans-serif",
   displayFontSize: "3rem",
   headlineFontSize: "2rem",
-  titleFontSize: "1.75rem",
-  bodyFontSize: "1.25rem",
+  titleFontSize: "1.25rem",
+  bodyFontSize: "0.875rem",
   mainFontRegular: 400,
   mainFontBold: 800,
 };
