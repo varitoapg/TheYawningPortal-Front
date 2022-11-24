acdc1305adfb8ec084c2c097769e1dc55ddc71da
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-24 20:57:39 +0100

    Test RegisterPage

---
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -0,0 +1,37 @@
+import { screen } from "@testing-library/react";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import { store } from "../../redux/store";
+import renderWithProviders from "../../testUtils/renderWithProvider";
+import RegisterPage from "./RegisterPage";
+
+describe("Given the RegisterPage page", () => {
+  const expectedUsername = "Username";
+  const expectedPassword = "Password";
+  const expectedEmail = "Email";
+  const expectedButtonText = "Register";
+  const expectedLinkText = "Log in";
+
+  describe("When it's rendered", () => {
+    test("Then it should return 2 inputs text element 'Username' and 'Email', a password input 'Password', 1 button 'Register', and a link 'Log in'", () => {
+      renderWithProviders(<RegisterPage />, { store });
+
+      const expectedInputUser = screen.getByRole("textbox", {
+        name: expectedUsername,
+      });
+      const passwordInput = screen.getByLabelText(expectedPassword);
+      const expectedInputEmail = screen.getByRole("textbox", {
+        name: expectedEmail,
+      });
+      const buttonRegister = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+      const linkLogin = screen.getByRole("link", { name: expectedLinkText });
+
+      expect(expectedInputUser).toBeInTheDocument();
+      expect(passwordInput).toBeInTheDocument();
+      expect(expectedInputEmail).toBeInTheDocument();
+      expect(buttonRegister).toBeInTheDocument();
+      expect(linkLogin).toBeInTheDocument();
+    });
+  });
+});
