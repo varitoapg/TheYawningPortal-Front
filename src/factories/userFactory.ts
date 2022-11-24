c4fdfad772c973cd27be31f019563afa7478d458
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-24 18:02:24 +0100

    Remove unnecessary Factory with all user information

--- src/factories/userFactory.ts
+++ src/factories/userFactory.ts
@@ -1,18 +1,9 @@
 import { faker } from "@faker-js/faker";
 import { Factory } from "fishery";
 import {
-  UserAllInformation,
   UserRegisterCredentials,
   UserState,
 } from "../redux/features/userSlice/types";
-
-const WithAllInformation = Factory.define<UserAllInformation>(() => ({
-  username: faker.internet.userName(),
-  password: faker.internet.password(12),
-  id: faker.random.alphaNumeric(10),
-  email: faker.internet.email(),
-  token: faker.random.alphaNumeric(10),
-}));
 
 const randomUserState = Factory.define<UserState>(() => ({
   id: faker.random.alphaNumeric(10),
@@ -27,6 +18,5 @@ const randomUserCredentials = Factory.define<UserRegisterCredentials>(() => ({
   username: faker.internet.userName(),
 }));
 
-export const getRandomUserWithAllInformation = () => WithAllInformation.build();
 export const getRandomUserState = () => randomUserState.build();
 export const getRandomUserCredentials = () => randomUserCredentials.build();
