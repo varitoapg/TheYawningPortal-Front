ed54b58f60683a5e2fd88fcc6f5805c4c40ae3f8
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-24 22:07:57 +0100

    Create and test LoginPage

---
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/react";
+import { store } from "../../redux/store";
+import renderWithProviders from "../../testUtils/renderWithProvider";
+import LoginPage from "./LoginPage";
+
+describe("Given the LoginPage page", () => {
+  const expectedUsername = "Username";
+  const expectedPassword = "Password";
+  const expectedButtonText = "Log in";
+  const expectedLinkText = "Register";
+
+  describe("When it's rendered", () => {
+    test("Then it should return 2 inputs text element 'Username' and 'Email', a password input 'Password', 1 button 'Log in', and a link 'Register'", () => {
+      renderWithProviders(<LoginPage />, { store });
+
+      const expectedInputUser = screen.getByRole("textbox", {
+        name: expectedUsername,
+      });
+      const passwordInput = screen.getByLabelText(expectedPassword);
+      const buttonLogin = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+      const linkLogin = screen.getByRole("link", { name: expectedLinkText });
+
+      expect(expectedInputUser).toBeInTheDocument();
+      expect(passwordInput).toBeInTheDocument();
+      expect(buttonLogin).toBeInTheDocument();
+      expect(linkLogin).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/LoginFom/LoginForm";
+import LoginPageStyled from "./LoginPageStyled";
+
+const LoginPage = () => {
+  return (
+    <LoginPageStyled>
+      <LoginForm />
+    </LoginPageStyled>
+  );
+};
+
+export default LoginPage;
