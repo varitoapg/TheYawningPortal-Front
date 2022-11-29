ff97192d5204e7655b22345384d37a7226226a0b
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-29 16:16:22 +0100

    Fix broken Header test also add logged option in the same test

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -7,11 +7,11 @@ describe("Given the LoginPage page", () => {
   const expectedUsername = "Username";
   const expectedPassword = "Password";
   const expectedButtonText = "Log in";
-  const expectedLinkToRegister = "Go to Register";
-  const expectedLinkToLogin = "Go to Login";
+  const expectedLinkToRegister = "Go to register";
+  const expectedLinkToLogin = "Go to login";
 
   describe("When it's rendered", () => {
-    test("Then it should return 2 inputs text element 'Username' and 'Email', a password input 'Password', 1 button 'Log in', and a link 'Register'", () => {
+    test.only("Then it should return 2 inputs text element 'Username' and 'Email', a password input 'Password', 1 button 'Log in', and a link 'Register'", () => {
       renderWithProviders(<LoginPage />, { store });
 
       const expectedInputUser = screen.getByRole("textbox", {
@@ -21,8 +21,12 @@ describe("Given the LoginPage page", () => {
       const buttonLogin = screen.getByRole("button", {
         name: expectedButtonText,
       });
-      const linkToRegister = screen.queryByLabelText(expectedLinkToRegister);
-      const linkToLogin = screen.queryByLabelText(expectedLinkToLogin);
+      const linkToRegister = screen.getByRole("link", {
+        name: expectedLinkToRegister,
+      });
+      const linkToLogin = screen.getByRole("link", {
+        name: expectedLinkToLogin,
+      });
 
       expect(expectedInputUser).toBeInTheDocument();
       expect(passwordInput).toBeInTheDocument();
