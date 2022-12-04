a159c0c792b1ebbd9b00395561107348f59f5075
Alvaro Parada <alvaro.parada92@gmail.com>
2022-12-04 15:09:54 +0100

    Style CreateForm, add arrow svg

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaDiceD20 } from "react-icons/fa";
 import useCharacter from "../../hooks/useCharacter/useCharacter";
 import { CharacterForm } from "../../redux/features/characterSlice/reducer/types";
 import Button from "../Button/Button";
@@ -64,243 +65,282 @@ const CreateForm = (): JSX.Element => {
   };
 
   return (
-    <CreateFormStyled>
-      <form className="create-form" onSubmit={handleSubmit}>
+    <CreateFormStyled onSubmit={handleSubmit}>
+      <div className="create-form__item--image">
+        <label className="create-form__label" htmlFor="image">
+          {(createCharacterData.image as File).name ? (
+            <img
+              src={URL.createObjectURL(createCharacterData.image as File)}
+              alt="Your avatar"
+              className="edit-profile__image"
+              aria-label="Your avatar"
+            />
+          ) : (
+            <div
+              aria-label="Empty image"
+              className={"edit-profile__image"}
+            ></div>
+          )}
+        </label>
+        <input
+          className="create-form__input--image"
+          type="file"
+          id="image"
+          onChange={handleFormChange}
+        />
+      </div>
+
+      <div className="character-container">
         <div className="create-form__item">
-          <label className="create-form__label" htmlFor="image">
-            {(createCharacterData.image as File).name ? (
-              <img
-                src={URL.createObjectURL(createCharacterData.image as File)}
-                alt="Your avatar"
-                className="edit-profile__image"
-                aria-label="Your avatar"
-              />
-            ) : (
-              <div
-                aria-label="Empty image"
-                className={"edit-profile__image"}
-              ></div>
-            )}
+          <label className="create-form__label--character" htmlFor="name">
+            name:
           </label>
           <input
-            className="create-form__input--image"
-            type="file"
-            id="image"
+            value={createCharacterData.name}
+            className="create-form__input"
+            type="text"
+            id="name"
+            placeholder="Insert name here"
             onChange={handleFormChange}
             autoComplete="off"
           />
         </div>
+        <div className="create-form__item">
+          <label className="create-form__label--character" htmlFor="race">
+            race:
+          </label>
+          <input
+            value={createCharacterData.race}
+            className="create-form__input"
+            type="text"
+            id="race"
+            placeholder="Insert race here"
+            onChange={handleFormChange}
+            autoComplete="off"
+          />
+        </div>
+        <div className="create-form__item">
+          <label
+            className="create-form__label--character"
+            htmlFor="characterClass"
+          >
+            class:
+          </label>
+          <select
+            className="create-form__input create-form__input--select"
+            onChange={handleFormChange}
+            id="characterClass"
+          >
+            <option>select a class</option>
+            <option value="artificer">artificer</option>
+            <option value="barbarian">barbarian</option>
+            <option value="warlock">warlock</option>
+            <option value="fighter">fighter</option>
+            <option value="monk">monk</option>
+            <option value="druid">druid</option>
+            <option value="rogue">rogue</option>
+            <option value="cleric">cleric</option>
+            <option value="ranger">ranger</option>
+            <option value="wizard">wizard</option>
+            <option value="sorcerer">sorcerer</option>
+            <option value="bard">bard</option>
+            <option value="paladin">paladin</option>
+          </select>
+        </div>
+      </div>
 
-        <div className="character-container">
-          <div className="create-form__item">
-            <label className="create-form__label" htmlFor="name">
-              name:
+      <div className="secondary-stats">
+        <div className="secondary-stats-container">
+          <h3 className="secondary-stats-title">armor class</h3>
+          <h2 className="secondary-stats-value">16</h2>
+        </div>
+        <div className="secondary-stats-container">
+          <h3 className="secondary-stats-title">initiative</h3>
+          <h2 className="secondary-stats-value">+2</h2>
+        </div>
+        <div className="secondary-stats-container">
+          <div className="secondary-stats__item">
+            <label className="secondary-stats__label" htmlFor="speed">
+              speed
             </label>
             <input
-              value={createCharacterData.name}
-              className="create-form__input"
-              type="text"
-              id="name"
-              placeholder="Placeholder"
+              className="secondary-stats__input"
+              type="number"
+              id="speed"
+              placeholder="5"
               onChange={handleFormChange}
               autoComplete="off"
             />
           </div>
-          <div className="create-form__item">
-            <label className="create-form__label" htmlFor="race">
-              race:
-            </label>
-            <input
-              value={createCharacterData.race}
-              className="create-form__input"
-              type="text"
-              id="race"
-              placeholder="Placeholder"
-              onChange={handleFormChange}
-              autoComplete="off"
-            />
-          </div>
-          <div className="create-form__item">
-            <label className="create-form__label" htmlFor="characterClass">
-              class:
-            </label>
-            <select
-              className="create-form__input"
-              onChange={handleFormChange}
-              id="characterClass"
-            >
-              <option>select a class</option>
-              <option value="artificer">artificer</option>
-              <option value="warlock">warlock</option>
-              <option value="fighter">fighter</option>
-              <option value="monk">monk</option>
-              <option value="druid">druid</option>
-              <option value="rogue">rogue</option>
-              <option value="cleric">cleric</option>
-              <option value="ranger">ranger</option>
-              <option value="wizard">wizard</option>
-              <option value="sorcerer">sorcerer</option>
-              <option value="bard">bard</option>
-              <option value="paladin">paladin</option>
-            </select>
-          </div>
+        </div>
+      </div>
+
+      <div className="main-stats">
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="strength">
+            strength
+          </label>
+          <input
+            className="main-stats__input"
+            type="number"
+            id="strength"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+            max={20}
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
         </div>
 
-        <div className="secondary-stats">
-          <div className="secondary-stats-container">
-            <h3 className="secondary-stats-title">armor class</h3>
-            <h2 className="secondary-stats-value">16</h2>
-          </div>
-          <div className="secondary-stats-container">
-            <h3 className="secondary-stats-title">initiative</h3>
-            <h2 className="secondary-stats-value">+2</h2>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="speed">
-                speed
-              </label>
-              <input
-                className="secondary-stats__input"
-                type="number"
-                id="speed"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-              />
-            </div>
-          </div>
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="constitution">
+            constitution
+          </label>
+          <input
+            max={20}
+            min={0}
+            className="main-stats__input"
+            type="number"
+            id="constitution"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
         </div>
 
-        <div className="main-stats">
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="strength">
-                strength
-              </label>
-              <input
-                className="secondary-stats__input"
-                type="number"
-                id="strength"
-                placeholder={`${createCharacterData.strength}`}
-                onChange={handleFormChange}
-                autoComplete="off"
-                max={20}
-              />
-            </div>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="constitution">
-                constitution
-              </label>
-              <input
-                max={20}
-                className="secondary-stats__input"
-                type="number"
-                id="constitution"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-              />
-            </div>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="dexterity">
-                dexterity
-              </label>
-              <input
-                max={20}
-                className="secondary-stats__input"
-                type="number"
-                id="dexterity"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-              />
-            </div>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="intelligence">
-                intelligence
-              </label>
-              <input
-                className="secondary-stats__input"
-                type="number"
-                id="intelligence"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-                max={20}
-              />
-            </div>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="wisdom">
-                wisdom
-              </label>
-              <input
-                className="secondary-stats__input"
-                type="number"
-                id="wisdom"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-                max={20}
-              />
-            </div>
-          </div>
-          <div className="secondary-stats-container">
-            <div className="secondary-stats__item">
-              <label className="secondary-stats__label" htmlFor="charisma">
-                charisma
-              </label>
-              <input
-                className="secondary-stats__input"
-                type="number"
-                id="charisma"
-                placeholder="5"
-                onChange={handleFormChange}
-                autoComplete="off"
-                max={20}
-              />
-            </div>
-          </div>
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="dexterity">
+            dexterity
+          </label>
+          <input
+            max={20}
+            min={0}
+            className="main-stats__input"
+            type="number"
+            id="dexterity"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
         </div>
 
-        <div className="character-information-container">
-          <div className="character-information-container__item">
-            <label
-              className="character-information-container__label"
-              htmlFor="background"
-            >
-              background:
-            </label>
-            <input
-              className="character-information-container__input"
-              type="text"
-              id="background"
-              placeholder="Placeholder"
-              onChange={handleFormChange}
-              autoComplete="off"
-            />
-          </div>
-          <div className="character-information-container__item">
-            <label
-              className="character-information-container__label"
-              htmlFor="details"
-            >
-              details:
-            </label>
-            <textarea id="details" rows={10} onChange={handleFormChange} />
-          </div>
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="intelligence">
+            intelligence
+          </label>
+          <input
+            className="main-stats__input"
+            type="number"
+            id="intelligence"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+            max={20}
+            min={0}
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
         </div>
 
-        <Button children={"Create"} ariaLabel="Create" />
-      </form>
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="wisdom">
+            wisdom
+          </label>
+          <input
+            className="main-stats__input"
+            type="number"
+            id="wisdom"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+            max={20}
+            min={0}
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
+        </div>
+
+        <div className="main-stats__item">
+          <label className="main-stats__label" htmlFor="charisma">
+            charisma
+          </label>
+          <input
+            className="main-stats__input"
+            type="number"
+            id="charisma"
+            placeholder="5"
+            onChange={handleFormChange}
+            autoComplete="off"
+            max={20}
+            min={0}
+          />
+          <div className="main-stats__modifier">{"+4"}</div>
+          <Button
+            children={<FaDiceD20 className="icon--randomize" />}
+            ariaLabel="Create"
+            classname="button button--randomize"
+          />
+        </div>
+      </div>
+
+      <div className="character-information-container">
+        <div className="character-information__item">
+          <label className="character-information__label" htmlFor="background">
+            background:
+          </label>
+          <input
+            className="character-information__input"
+            type="text"
+            id="background"
+            placeholder="Insert your background here"
+            onChange={handleFormChange}
+            autoComplete="off"
+          />
+        </div>
+        <div className="character-information__item">
+          <label className="character-information__label" htmlFor="details">
+            details:
+          </label>
+          <textarea
+            id="details"
+            placeholder="Insert your details here"
+            rows={10}
+            onChange={handleFormChange}
+            className="character-information__input character-information__input--textArea"
+          />
+        </div>
+      </div>
+
+      <Button
+        classname="button button-create"
+        children={"Create"}
+        ariaLabel="Create"
+      />
     </CreateFormStyled>
   );
 };
