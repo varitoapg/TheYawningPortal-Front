7757793cb472f62889286d08ee1d9741e1b38050
Alvaro Parada <alvaro.parada92@gmail.com>
2022-12-04 18:25:05 +0100

    Add currentCharacter to state, remove svg from coverage

--- src/mocks/states/characterState.ts
+++ src/mocks/states/characterState.ts
@@ -1,13 +1,19 @@
-import { getRandomCharacterList } from "../../factories/characterFactory";
+import {
+  getRandomCharacter,
+  getRandomCharacterList,
+} from "../../factories/characterFactory";
 import {
   Character,
+  CharacterForm,
   CharacterState,
 } from "../../redux/features/characterSlice/reducer/types";
 
 export const initialCharacterState: CharacterState = {
   characters: [] as Character[],
+  currentCharacter: {} as CharacterForm,
 };
 
 export const fourCharactersState: CharacterState = {
   characters: getRandomCharacterList(4),
+  currentCharacter: getRandomCharacter(),
 };
