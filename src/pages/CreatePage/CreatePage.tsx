785b21a51aa975b51a452b99ee109664b1770630
Alvaro Parada <alvaro.parada92@gmail.com>
2022-12-07 10:38:33 +0100

    Reset filter when change from HomePage

--- src/pages/CreatePage/CreatePage.tsx
+++ src/pages/CreatePage/CreatePage.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import CreateForm from "../../components/CreateForm/CreateForm";
 import Header from "../../components/Header/Header";
+import { filterClassActionCreator } from "../../redux/features/uiSlice/uiSlice";
+import { useAppDispatch } from "../../redux/hooks";
 import CreatePageStyled from "./CreatePageStyled";
 
 const CreatePage = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(filterClassActionCreator("all"));
+  }, [dispatch]);
   return (
     <>
       <Header />
