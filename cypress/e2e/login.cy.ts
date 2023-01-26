ba8b786da1814334dda79b20dcda4bed36814d89
Alvaro Parada <alvaro.parada92@gmail.com>
2023-01-26 12:25:55 +0100

    Test login page

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,13 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("/");
+describe("Given The Yawning Portal's login page", () => {
+  describe("When the user types 'SuperAdmin' in username input and 'SuperAdmin' in password input, and clicks in Log in button", () => {
+    it("Then it should go to Home page", () => {
+      cy.visit("/");
+      cy.findByRole("textbox", { name: "Username" }).type("superAdmin");
+      cy.findByLabelText("Password").type("superAdmin");
+      cy.findByRole("button", { name: "Log in" }).click();
+
+      cy.findByRole("combobox", { name: "class:" }).should("exist");
+      cy.url().should("include", "/home");
+    });
   });
 });
